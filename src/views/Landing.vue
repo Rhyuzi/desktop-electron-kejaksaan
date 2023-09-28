f0773598b22c76e757a910f1b23f4792b85bf0f2
unknown <azi>
2023-09-28 22:10:48 +0700

    azi - project desktop jumpa

---
+++ src/views/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="kontener-landing">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-8 mx-auto text-center">
+                <img alt="Logo Kejaksaan" class="pb-2" src="../assets/logo-kejaksaan.png">
+                <h1 class="fw-bold name-app">VICA</h1>
+                <p class="text-muted pb-4 font-14">Video Conference Adhyaksa</p>
+                <router-link class="w-100 btn btn-landing-join mb-3" to="/gabungrapat">Gabung Rapat</router-link>
+                <a href="#" class="w-100 btn btn-landing-signin">Masuk</a>
+            </div>
+            <div class="text-center fixed-bottom p2">
+                <div class="text-center p-3 text-secondary">
+                    V1.0.0
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/PilihServer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="d-flex flex-column justify-content-between align-items-center px-50 py-40">
+		<div class="">
+			<h2 class="text-black-gray">Pilih Server</h2>
+		</div>
+        <form class="w-100 d-flex flex-column mt-30">
+            <label class="serverlabel">
+                <input type="radio" name="radioserver" class="server-radiobtn" checked>
+                <span class="servername w-100">Kejaksaan Agung <i class="fas fa-check"></i></span>
+            </label>
+            <label class="serverlabel">
+                <input type="radio" name="radioserver" class="server-radiobtn">
+                <span class="servername w-100">Kejati Jawa Timur <i class="fas fa-check"></i></span>
+            </label>
+            <label class="serverlabel">
+                <input type="radio" name="radioserver" class="server-radiobtn">
+                <span class="servername w-100">Kejati Sulawesi Selatan <i class="fas fa-check"></i></span>
+            </label>
+            <label class="serverlabel">
+                <input type="radio" name="radioserver" class="server-radiobtn">
+                <span class="servername w-100">Kejati Kalimantan Selatan <i class="fas fa-check"></i></span>
+            </label>
+            <label class="serverlabel">
+                <input type="radio" name="radioserver" class="server-radiobtn">
+                <span class="servername w-100">Kejati Sumatera Utara <i class="fas fa-check"></i></span>
+            </label>
+        </form>
+		<div class="d-flex justify-content-between align-items-center mt-92 w-100">
+			<a @click="$router.go(-1)" class="link-text">
+				<i class="fas fa-chevron-left"></i>
+				Kembali
+			</a>
+			<button type="submit" class="btn btn-primary-kejaksaan">SIMPAN</button>
+		</div>
+	</div>
+</template>
